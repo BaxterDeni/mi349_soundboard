93020793a82de6cadf782cc23dc27252f725fe8a
Baxter Deni <baxterpdeni@gmail.com>
2021-11-18 15:59:25 -0500

    master

---
+++ soundboard.js
@@ -0,0 +1,17 @@
+var splashButton = document.getElementById('splashbtn')
+var fireButton = document.getElementById('firebtn')
+var ducksButton = document.getElementById('ducksbtn')
+
+splashButton.addEventListener('click', function () {
+    console.log('Splash clicked!')
+  })
+
+fireButton.addEventListener('click', function () {
+    console.log('Fire clicked!')
+  })
+
+ducksButton.addEventListener('click', function () {
+    console.log('Ducks clicked!')
+  })
+
+  
